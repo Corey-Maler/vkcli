d10beed2b6844f0ed6fcf14c8571d18e3ddc6517
Corey <corey.maler@outlook.com>
2014-08-17 15:21:37 +0400

    saving access token

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var open = require('open');
+var Q = require('q');
+var request = require('request');
+var config = require('./lib/config');
 
 var readline = require('readline');
 
@@ -15,6 +18,30 @@ var app =
     appSecret: 'OmP8REoS6fVjnRW8wZ92',
     access_token: null
 }
+
+var VK = function(_app)
+{
+    var app = _app;
+
+    return {
+        req: function()
+        {
+            var def = Q.defer();
+
+            var uri = "https://api.vk.com/method/getProfiles?uid=66748&access_token="+app.access_token;
+
+            request(uri, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body) // Print the google web page.
+                }
+            });
+
+            return def.promise;
+        }
+    }
+};
+
+
 
 var connect = function()
 {
@@ -36,12 +63,24 @@ var connect = function()
     page += "&v=5.24";
     page += "&response_type=token";
 
-    /*
     open(page, function(err, a, b)
     {
         if (err) throw err;
     });
-    */
+
+    rl.question("Please, copy URI of opened page and paste: ", function(login)
+    {
+        var reg = /access_token=(.+)&expires_in=(.+)&user_id=(.+)$/i;
+        var a = login.match(reg);
+
+        app.access_token = a[1];
+        config.set('access_token', app.access_token);
+        console.log(a);
+        console.log('Your page: ', login);
+
+        var vk = VK(app);
+        vk.req();
+    });
 };
 
 rl.on('line', function(cmd)
@@ -49,12 +88,8 @@ rl.on('line', function(cmd)
    console.log(cmd);
 });
 
-connect();
-rl.question("Please, copy URI of opened page and paste: ", function(login)
+if (typeof config.get('access_token') == "undefined")
 {
-    var reg = /access_token=(.+)&expires_in=(.+)&user_id=(.+)$/i;
-    var a = login.match(reg);
-    console.log(a);
-    console.log('Your page: ', login);
-});
+    connect();
+}
 
