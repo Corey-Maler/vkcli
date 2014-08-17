359acb64f79c13e7f10179254f78ed9cbacaa5fd
Corey <corey.maler@outlook.com>
2014-08-17 12:14:49 +0400

    Просим ввести логин-пароль

--- app.js
+++ app.js
@@ -9,7 +9,29 @@ var rl = readline.createInterface(
         terminal: false
     });
 
+var acc = {login: null, pass: null};
+var connect = function(acc)
+{
+
+};
+
 rl.on('line', function(cmd)
 {
    console.log(cmd);
 });
+
+rl.question("Enter you vk login: ", function(login)
+{
+    console.log('You login: ', login);
+    acc.login = login;
+    rl.question("Your password: ", function(pass)
+    {
+        console.log('Your password: ', pass);
+        acc.pass = pass;
+
+        console.log('Trying to connect...');
+        connect(acc);
+
+    });
+});
+
