aaf302a121446c69334a2dda2372c8a8a2b50c88
Corey <corey.maler@outlook.com>
2014-08-17 13:57:00 +0400

    быстро выяснили, что так не катит

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var vksdk = require('vksdk');
+var vk = new vksdk({appID: 4509664, appSecret: 'OmP8REoS6fVjnRW8wZ92', 'mode': "oauth"});
 
 var readline = require('readline');
 
@@ -12,7 +13,23 @@ var rl = readline.createInterface(
 var acc = {login: null, pass: null};
 var connect = function(acc)
 {
+    vk.acquireToken(acc.login, acc.pass);
+    vk.on('appServerTokenReady', function() {
+        vk.request('acquireTokenReady');
+        // etc
 
+        console.log('connected');
+
+
+    });
+
+    vk.request('getProfiles', {'uids' : '29894'});
+    vk.on('done:getProfiles', function(_o) {
+        console.log(_o);
+    });
+    vk.on('acquireTokenNotReady', function(_error) {
+        // error handler
+    });
 };
 
 rl.on('line', function(cmd)
