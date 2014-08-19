c30acd2de3ceeef6b5c26598aeb37b74d2c73443
Corey <corey.maler@outlook.com>
2014-08-20 00:19:26 +0400

    vim-style управление

--- screen.js
+++ screen.js
@@ -6,14 +6,15 @@ var program = blessed.program();
 
 
 
-var box = blessed.box(
+var box = blessed.list(
     {
         top: 0,
         left: 0,
         width: 30,
         height: screen.height - 1,
-        content: "hello {bold}world{/bold}!",
         tags: true,
+        selectedBg: "white",
+        selectedFg: "black",
         border:
         {
             type: 'line'
@@ -24,10 +25,6 @@ var box = blessed.box(
             border:
             {
                 fg: "#f0f0f0"
-            },
-            hover:
-            {
-                bg: "green"
             }
         }
     }
@@ -47,14 +44,10 @@ var chat = blessed.box(
         },
         style:
         {
-            fg: "red",
+            fg: "white",
             border:
             {
                 fg: "#f0f0f0"
-            },
-            hover:
-            {
-                bg: "green"
             }
         }
     }
@@ -75,14 +68,10 @@ var input = blessed.box(
         },
         style:
         {
-            fg: "red",
+            fg: "white",
             border:
             {
                 fg: "#f0f0f0"
-            },
-            hover:
-            {
-                bg: "green"
             }
         }
     }
@@ -104,52 +93,111 @@ screen.append(chat);
 screen.append(input);
 screen.append(status);
 
-screen.key([':'], function(ch, key)
-{
-    box.insertLine(1, 'foo');
-    screen.render();
-});
-
 var message = "";
+var selected = 0;
 
-// modes: 0 - normal, 1 - insert
+// modes: 0 - normal, 1 - insert, 2 - command
 var mode = 0;
+
+var command = "";
+var commands =
+{
+    'q': function()
+    {
+        return process.exit(0);
+    }
+}
+var showUnknownCommand = null;
+
+var hotkeys =
+{
+    'i': function()
+    {
+        mode = 1;
+    },
+    ':': function()
+    {
+        mode = 2;
+    },
+    'up': function()
+    {
+        selected --;
+        if (selected < 0)
+            selected = contactList.length - 1;
+    },
+    'down': function()
+    {
+        selected ++;
+        if (selected > contactList.length - 1)
+            selected = 0;
+    }
+
+}
 
 program.on('keypress', function(ch, key)
 {
-    if (mode == 0)
+    switch (mode)
     {
-        switch (ch)
-        {
-            case 'i':
-                mode = 1;
-                break;
-
-        }
-
-        switch (key.name)
-        {
-            case 'up':
-                break;
-            case 'down':
-                break;
-        }
-    }
-    else
-    {
-        switch (key.name)
-        {
-            case 'escape':
-                mode = 0;
-                break;
-            case 'enter':
-                mode = 0;
-                message = "";
-                break;
-            default :
-                message += ch;
-                break;
-        }
+        case 0:
+            if (typeof ch != "undefined" && typeof hotkeys[ch] == "function")
+            {
+                showUnknownCommand = null;
+                hotkeys[ch]();
+            }
+            else
+            {
+                if (typeof hotkeys[key.name] == "function")
+                {
+                    showUnknownCommand = null;
+                    hotkeys[key.name]();
+                }
+            }
+            break;
+        case 1:
+            switch (key.name)
+            {
+                case 'escape':
+                    mode = 0;
+                    break;
+                case 'enter':
+                    mode = 0;
+                    message = "";
+                    break;
+                default :
+                    if (typeof ch == "undefined") break;
+                    message += ch;
+                    break;
+            }
+            break;
+        case 2:
+            switch (key.name)
+            {
+                case 'escape':
+                    mode = 0;
+                    command = 0;
+                    break;
+                case 'enter':
+                    mode = 0;
+                    // todo: parse args
+                    if (command.length > 0)
+                    {
+                        if (typeof commands[command] != "undefined")
+                        {
+                            commands[command]();
+                        }
+                        else
+                        {
+                            showUnknownCommand = util.format("{red-fg}Unknown command: %s{/red-fg}", command);
+                        }
+                    }
+                    command = "";
+                    break;
+                default :
+                    if (typeof ch == "undefined") break;
+                    command += ch;
+                    break;
+            }
+            break;
     }
     redraw();
 });
@@ -160,38 +208,56 @@ screen.key(['C-c'], function(ch, key) {
 
 box.focus();
 
-screen.render();
-
 var contactList = [];
+var vk = null;
 
 var redraw = function()
 {
-
-    status.setContent('Mode: '+(mode == 0? "{green-fg}normal{/green-fg}":"{red-fg}insert{/red-fg}"));
-
-    box.setContent('{bold}Contacts:{/bold}');
+    if (mode == 2)
+    {
+        status.setContent(':'+command);
+    }
+    else
+    {
+        if (showUnknownCommand != null)
+        {
+            status.setContent(showUnknownCommand);
+        }
+        else
+        {
+            status.setContent('Mode: '+(mode == 0? "{green-fg}normal{/green-fg}":"{red-fg}insert{/red-fg}"));
+        }
+    }
+    box.clearItems();
+    //box.setContent('{bold}Contacts:{/bold}');
     for (var i in contactList)
     {
+        if (!contactList.hasOwnProperty(i)) break;
         var contact = contactList[i];
         var str = util.format('%s %s %s %s',
             (contact.online == 1? "{green-fg}o{/green-fg}":"f"),
             contact.first_name,
             contact.last_name,
             (contact.unread > 0? contact.unread: " "));
-        box.pushLine(str);
+        box.addItem(str);
 
-        var line = blessed.line({
-            content: "sdds"
-        })
-        box.pushLine(line);
     }
+
+    box.select(selected);
 
     input.setContent(message);
     screen.render();
 }
 
+redraw();
+
 var c =
 {
+    setVK: function(_vk)
+    {
+        vk = _vk;
+        redraw();
+    },
     setContactList: function(cl)
     {
         contactList = cl;
